4bc856ee6e3189b7f59b38c56a2a383e4732e61f
Jakub Bibro <jakub.bibro@allegro.pl>
2022-01-20 18:21:34 +0100

    ktlint

--- src/main/kotlin/com/github/jbibro/cinema/movie/data/MovieRepository.kt
+++ src/main/kotlin/com/github/jbibro/cinema/movie/data/MovieRepository.kt
@@ -4,4 +4,3 @@ import com.github.jbibro.cinema.movie.infrastructure.CustomizedMovieRepository
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
 interface MovieRepository : ReactiveMongoRepository<MongoMovie, String>, CustomizedMovieRepository
-
